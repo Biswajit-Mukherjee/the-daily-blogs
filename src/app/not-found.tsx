0f5e67a84bcc7e0eeb5af4ae2fc0b8bcd0c4d633
Biswajit <biswajitmukherjee549@gmail.com>
2025-04-13 17:05:22 +0530

    fix: fix 404 padding issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -25,7 +25,7 @@ const NotFound: NextPage = () => {
         <section className="w-full max-w-5xl mx-auto mt-12 mb-24 grid gap-5 items-center justify-center">
           <div
             data-uia="bg-image"
-            className="w-[440px] h-[320px] mx-auto aspect-auto relative"
+            className="w-[420px] h-[300px] mx-auto aspect-auto relative"
           >
             <Image
               className="w-full h-full object-cover aspect-auto"
@@ -38,27 +38,32 @@ const NotFound: NextPage = () => {
             />
           </div>
 
-          <h3
-            data-uia="error-code"
-            className="w-fit mx-auto mt-2.5 align-middle px-4 py-2.5 border-l-2 border-l-red-500 text-center text-xl leading-normal font-light antialiased prose dark:prose-invert"
+          <div
+            data-uia="404-content"
+            className="w-full p-10 grid items-center mx-auto"
           >
-            Error Code <strong>404</strong>
-          </h3>
+            <h3
+              data-uia="error-code"
+              className="w-fit mx-auto mt-2.5 align-middle px-4 py-2.5 border-l-2 border-l-red-500 text-center text-xl leading-normal font-light antialiased prose dark:prose-invert"
+            >
+              Error Code <strong>404</strong>
+            </h3>
 
-          <p
-            data-uia="error-message"
-            className="w-full text-center text-base font-normal leading-normal antialiased prose dark:prose-invert"
-          >
-            The page you&apos;re looking for could not be found.
-          </p>
+            <p
+              data-uia="error-message"
+              className="w-full p-5 text-center text-base font-normal leading-normal antialiased prose dark:prose-invert"
+            >
+              The page you&apos;re looking for could not be found.
+            </p>
 
-          <Button
-            className="w-full h-14 shadow-2xl mt-6 p-5 bg-primary text-white text-lg leading-none antialiased"
-            data-uia="cta-btn"
-            role="link"
-          >
-            <Link href="/">Go Home</Link>
-          </Button>
+            <Button
+              className="w-full h-14 shadow-2xl mt-6 mx-auto p-5 bg-primary text-white text-lg leading-none antialiased"
+              data-uia="cta-btn"
+              role="link"
+            >
+              <Link href="/">Go Home</Link>
+            </Button>
+          </div>
         </section>
       </main>
     </>
